ec8b0f3f0e967a7ce94d761af73652d14b5927ae
unixpup <dev@fault.wtf>
2025-02-13 10:49:59 -0300

    erm

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/* Entry point for program int main(void)
+			   {
+			   	return (0);
+			   }
+*/
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle"); // Print a string to the STDIO.
+	// Return 0 to indicate success.
 	return (0);
 }
