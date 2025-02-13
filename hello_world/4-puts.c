33c0c0e073f7ac091700f1a00434a7ea2107f3b8
unixpup <dev@fault.wtf>
2025-02-13 10:54:57 -0300

    Fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -8,8 +8,6 @@
 
 int main(void)
 {
-	// Use the puts function to print a string to the console.
 	puts("\"Programming is like building a multilingual puzzle");
-	// Return 0 to indicate success.
 	return (0);
 }
