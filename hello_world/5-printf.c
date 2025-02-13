3193147bea5330c1e7c2d19608dff01fca68224b
unixpup <dev@fault.wtf>
2025-02-13 10:56:40 -0300

    Printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This is the main entry point for our program.
+ * Return: This should return 0.
+*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
