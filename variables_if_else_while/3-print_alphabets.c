6dd442c9b24d5171040797aca3dfffe8e15d5440
unixpup <dev@fault.wtf>
2025-02-18 11:28:15 -0300

    Alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void) 
+{
+	char ch;
+
+    	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+    	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+    	putchar('\n');
+
 	return (0);
 }
