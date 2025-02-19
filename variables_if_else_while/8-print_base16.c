0953767f8adea861c5b959bc8ccc7b9b727c63ff
unixpup <dev@fault.wtf>
2025-02-19 09:57:37 -0300

    base16

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	int i;
+	for (i = 0; i < 16; i++)
+	{
+		if (i < 10) {
+			putchar('0' + i);
+		} else {
+			putchar('a' + (i - 10));
+		}
+	}
+    	putchar('\n');
+	return 0;
+}
