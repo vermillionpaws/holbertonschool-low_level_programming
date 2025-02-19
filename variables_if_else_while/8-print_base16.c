c2b00d69655b594e3fc9f80107a1172473e000a3
unixpup <dev@fault.wtf>
2025-02-19 09:58:47 -0300

    Dcs

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ * main - Entry point for program
+ *
+ * Description: print base16
+ *
+ * Return: Always 0 (Success) and base16
+ */
+
+int main(void)
 {
 	int i;
 	for (i = 0; i < 16; i++)
