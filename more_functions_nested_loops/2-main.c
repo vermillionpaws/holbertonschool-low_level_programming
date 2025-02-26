525abbe9a243b4df6dc60c1898975ea49368abd6
unixpup <dev@fault.wtf>
2025-02-26 15:33:24 -0300

    mul

---
+++ more_functions_nested_loops/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*	main - entry point
+*	multiply 2 numbers
+*	return: print multiplied numbers
+*/
+
+int main(void)
+{
+	printf("%d\n", mul(98, 1024));
+    	printf("%d\n", mul(-402, 4096));
+
+	return ();
+}
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+*	mul - entry point
+*	multiply 2 numbers
+*	return: multiplied numbers
+*/
+
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
