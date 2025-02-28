226d9a7a8eadd8a415d70a63dae1b2eb50f2827b
unixpup <dev@fault.wtf>
2025-02-28 09:58:40 -0300

    most numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints the numbers from 0 to 9
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+			i++;
+		_putchar(i);
+	}
+	_putchar('\n');
+}
