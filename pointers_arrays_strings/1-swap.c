0c59bcbf7acf5d8e73428bc8830e89f351c518cb
unixpup <dev@fault.wtf>
2025-02-28 10:35:16 -0300

    swap integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* swap int - swaps the values of two integers
+* @a: first integer
+* @b: second integer
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
