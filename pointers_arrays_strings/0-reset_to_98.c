d7396c42de8ddf89cd86a0c4f8c940f13dde66fa
unixpup <dev@fault.wtf>
2025-02-28 10:31:17 -0300

    Reset to 98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+* reset_to_98 - updates the value it points to 98
+* @n: pointer to the integer to be updated
+* Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
