67168187efc5e0c34fabb1b0d5ce957fa56e2d59
unixpup <dev@fault.wtf>
2025-03-06 11:47:36 -0300

    wawa

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @str: string to check
+ * @accept: string to check against
+ * Return: number of bytes in the initial segment of @str
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count;
+
+	while (*s) 
+	{
+		for (count = 0; accept[count]; count++)
+		{
+			if (*s == accept[count])
+				break;
+		}
+		if (!accept[count])
+			break;
+		s++;
+	}
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,4 @@ char *leet(char *);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
