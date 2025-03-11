a8b2ec7d79c86fc2f6d3bbff198d8e8665343869
unixpup <dev@fault.wtf>
2025-03-11 11:04:24 -0300

    Add docs, also make sure we have memory

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,13 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/** create_array - creates an array of chars, and initializes it with a specific char.
+* @size: size of the array
+* @c: char to initialize the array
+* Return: pointer to the array, or NULL if it fails
+*/
+
 char *create_array(unsigned int size, char c)
 {
 	char *str = malloc(size * sizeof(char));
 	unsigned int i;
-	for (i = 0; i < size; i++)
+
+	if(str == NULL || size == 0)
 	{
-		str[i] = c;
+		return (NULL);
+	} else
+	{
+		for (i = 0; i < size; i++)
+		{
+			str[i] = c;
+		}
 	}
+
 	return (str);
 }
