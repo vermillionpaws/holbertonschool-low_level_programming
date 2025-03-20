ecce898f0fe4c16ddda311a24bab0abe35499af4
unixpup <dev@fault.wtf>
2025-03-20 13:12:23 -0300

    awfdas

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: function to print name
- * Return: void
- */
+* print_name - prints a name
+* @name: name to print
+* @f: function to print name
+* Return: void
+*/
 
 void print_name(char *name, void (*f)(char *))
 {
