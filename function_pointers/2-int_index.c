82bc6e38616dd403baa2535d3aa1646362ec268d
unixpup <dev@fault.wtf>
2025-03-21 16:19:32 -0300

    test

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ *	int_index - searches for an integer
+ *	@array: array of integers
+ *	@size: size of the array
+ *	@cmp: pointer to the function to be used to compare values
+ *	Return: index of the first element for which cmp function does not
+ *return 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int)) {}
