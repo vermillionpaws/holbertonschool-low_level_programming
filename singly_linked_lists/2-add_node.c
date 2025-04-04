b26a94bfb3b2ffe81ae9d8924efff20e9e715f30
unixpup <dev@fault.wtf>
2025-04-04 12:48:40 -0300

    style: add add_node function and main test for linked list
    Signed-off-by: unixpup <dev@fault.wtf>

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ * add_node - Adds a new node at the beginning of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the new node.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+	unsigned int str_len = 0;
+
+	while (str[str_len])
+		str_len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->len = str_len;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
