cfb7df58ffc107c6d8b022d6ec8df6d12dd14310
unixpup <dev@fault.wtf>
2025-04-05 18:54:17 -0300

    Add docs

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,6 +1,17 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ * free_list - Frees a linked list and all its nodes
+ * @head: Pointer to the head of the list to be freed
+ *
+ * Description: This function iterates through a linked list starting from
+ * the head, freeing the string content of each node and then the node itself.
+ * It handles the case where the list is empty by returning immediately.
+ *
+ * Return: Nothing (void)
+ */
+
 void free_list(list_t *head) {
   list_t *temp;
 
